a31d898652d23243e8291ea5c9d7e7ad0fe49d85
Yusuf Idi Maina <120538505+yusufidimaina9989@users.noreply.github.com>
2024-08-25 19:50:32 +0100

    allow partial withdrawal at vaultCompleteWithdrawal.ts

--- src/contracts/vaultCompleteWithdrawal.ts
+++ src/contracts/vaultCompleteWithdrawal.ts
@@ -1,7 +1,9 @@
 import {
     assert,
     ByteString,
+    byteString2Int,
     hash256,
+    int2ByteString,
     method,
     prop,
     sha256,
@@ -10,20 +12,11 @@ import {
 } from 'scrypt-ts'
 import { SHPreimage, SigHashUtils } from './sigHashUtils'
 
-
 export class VaultCompleteWithdrawal extends SmartContract {
-
     @prop()
     sequenceVal: bigint
 
-    /**
-     *
-     * @param sequenceVal - Relative locktime of withdrawal period.
-     *
-     */
-    constructor(
-        sequenceVal: bigint
-    ) {
+    constructor(sequenceVal: bigint) {
         super(...arguments)
         this.sequenceVal = sequenceVal
     }
@@ -33,49 +26,65 @@ export class VaultCompleteWithdrawal extends SmartContract {
         shPreimage: SHPreimage,
         prevTxVer: ByteString,
         prevTxLocktime: ByteString,
-        prevTxInputContract: ByteString, // First input chunk should also include length prefix...
+        prevTxInputContract: ByteString,
         prevTxInputFee: ByteString,
         vaultSPK: ByteString,
         vaultAmt: ByteString,
+        withdrawalAmt: ByteString,
         targetSPK: ByteString,
         feePrevout: ByteString
     ) {
+        // Enforce relative locktime.
         this.csv(this.sequenceVal)
 
         // Check sighash preimage.
         const s = SigHashUtils.checkSHPreimage(shPreimage)
         assert(this.checkSig(s, SigHashUtils.Gx))
 
-        // Construct prev tx.
+        // Convert ByteString amounts to BigInt for arithmetic operations.
+        const vaultAmtInt = byteString2Int(vaultAmt)
+        const withdrawalAmtInt = byteString2Int(withdrawalAmt)
+
+        // Calculate remaining amount in the vault.
+        const remainingVaultAmt = vaultAmtInt - withdrawalAmtInt
+        assert(
+            remainingVaultAmt >= 0,
+            'Withdrawal amount exceeds vault balance.'
+        )
+
+        // Construct the previous tx.
         const dust = toByteString('2202000000000000')
         const prevTxId = hash256(
             prevTxVer +
-            prevTxInputContract +
-            prevTxInputFee +
-            toByteString('02') + vaultAmt + vaultSPK + dust + targetSPK +
-            prevTxLocktime
+                prevTxInputContract +
+                prevTxInputFee +
+                toByteString('02') +
+                withdrawalAmt +
+                targetSPK +
+                int2ByteString(remainingVaultAmt) +
+                vaultSPK +
+                dust +
+                prevTxLocktime
         )
 
         // Enforce prevouts.
         const hashPrevouts = sha256(
-            prevTxId + toByteString('00000000') +
-            feePrevout
+            prevTxId + toByteString('00000000') + feePrevout
         )
         assert(hashPrevouts == shPreimage.hashPrevouts, 'hashPrevouts mismatch')
 
-        // Enforce outputs
+        // Enforce outputs.
         const hashOutputs = sha256(
-            vaultAmt + targetSPK
+            withdrawalAmt +
+                targetSPK +
+                int2ByteString(remainingVaultAmt) +
+                vaultSPK
         )
         assert(hashOutputs == shPreimage.hashOutputs, 'hashOutputs mismatch')
     }
 
     @method()
     private csv(sequenceVal: bigint): void {
-        // ... Gets substituted for OP_CSV w/ inline assembly hook
-        // TODO: Rm once OP_CSV is added to compiler.
         assert(true)
     }
-
-    // Default taproot key spend must be disabled!
 }
