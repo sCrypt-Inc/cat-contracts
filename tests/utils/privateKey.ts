41c022cc746ab2df66179dba692f9cc6aaad0fda
Yusuf Idi Maina <120538505+yusufidimaina9989@users.noreply.github.com>
2024-09-08 18:26:47 +0100

    Gen BTC Address

--- tests/utils/privateKey.ts
+++ tests/utils/privateKey.ts
@@ -1,19 +1,19 @@
-import { bsv } from 'scrypt-ts'
+import { PrivateKey, PublicKey, crypto } from 'bitcore-lib'
 import * as dotenv from 'dotenv'
 import * as fs from 'fs'
 
-export function genPrivKey(network: bsv.Networks.Network): bsv.PrivateKey {
+export function genPrivKey(network: 'testnet'): PrivateKey {
     dotenv.config({
         path: '.env',
     })
 
     const privKeyStr = process.env.PRIVATE_KEY
-    let privKey: bsv.PrivateKey
+    let privKey: PrivateKey
     if (privKeyStr) {
-        privKey = bsv.PrivateKey.fromWIF(privKeyStr as string)
+        privKey = PrivateKey.fromWIF(privKeyStr as string)
         console.log(`Private key already present ...`)
     } else {
-        privKey = bsv.PrivateKey.fromRandom(network)
+        privKey = PrivateKey.fromRandom(network)
         console.log(`Private key generated and saved in "${'.env'}"`)
         console.log(`Publickey: ${privKey.publicKey}`)
         console.log(`Address: ${privKey.toAddress()}`)
@@ -27,10 +27,10 @@ export function genPrivKey(network: bsv.Networks.Network): bsv.PrivateKey {
     return privKey
 }
 
-export const myPrivateKey = genPrivKey(bsv.Networks.testnet)
+export const myPrivateKey = genPrivKey('testnet')
 
-export const myPublicKey = bsv.PublicKey.fromPrivateKey(myPrivateKey)
-export const myPublicKeyHash = bsv.crypto.Hash.sha256ripemd160(
+export const myPublicKey = PublicKey.fromPrivateKey(myPrivateKey)
+export const myPublicKeyHash = crypto.Hash.sha256ripemd160(
     myPublicKey.toBuffer()
 )
 export const myAddress = myPublicKey.toAddress()
